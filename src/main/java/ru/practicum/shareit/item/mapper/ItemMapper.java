1c724e52b460e48b0e520277df3ec31d71671db4
Eduard Gritsak <Hy6Jlo1998@yandex.ru>
2025-03-31 18:38:13 +0300

    feat: Add DTOs and mappers for Item and User

---
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.item.mapper;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+public final class ItemMapper {
+    public static Item MapToItem(ItemDto item) {
+        return Item.builder()
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .build();
+    }
+
+    public static ItemDto MapToItemDto(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .build();
+    }
+}
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Item {
+    Long id;
+    @NotBlank(message = "Имя не может быть пустым")
+    String name;
+    @NotBlank(message = "Описание не может быть пустым")
+    String description;
+    @NotNull(message = "Поле \"available\" не может быть пустым")
+    Boolean available;
+    Long owner;
 }
