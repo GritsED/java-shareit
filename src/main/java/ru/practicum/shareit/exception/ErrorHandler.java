880d81f9c563262516e16d689280aa7eee63d610
Eduard Gritsak <Hy6Jlo1998@yandex.ru>
2025-04-01 21:32:31 +0300

    Fix after review

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -19,4 +19,10 @@ public class ErrorHandler {
     public ErrorResponse handleConflict(final ConflictException e) {
         return new ErrorResponse(e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        return new ErrorResponse("An unexpected error occurred.");
+    }
 }
